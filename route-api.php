250f8c8ec6f4f9a2555b6d2e26720262f3020f80
Nadrak1 <nicourdu@gmail.com>
2022-11-14 18:40:55 -0300

    Elimino todo sobre category y dejo el inner join de la categoria

--- route-api.php
+++ route-api.php
@@ -14,12 +14,6 @@ $router->addRoute('videogame/:ID', 'DELETE', 'gameApiController', 'deleteGame');
 $router->addRoute('videogame', 'POST', 'gameApiController', 'createGame');
 $router->addRoute('videogame/:ID', 'PUT', 'gameApiController', 'editGame');
 
-//                      ---CATEGORY---
-$router->addRoute('category', 'GET', 'categoryApiController', 'showAllCategory');
-$router->addRoute('category/:ID', 'GET', 'categoryApiController', 'getCategory');
-$router->addRoute('category/:ID', 'DELETE', 'categoryApiController', 'deleteCategory');
-$router->addRoute('category', 'POST', 'categoryApiController', 'createCategorys');
-$router->addRoute('category/:ID', 'PUT', 'categoryApiController', 'editCategory');
 
 // rutea
 $router->route($_GET["resource"], $_SERVER['REQUEST_METHOD']);
